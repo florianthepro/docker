7bd9e0895cf7208e1ccaed897de8cae3d9e4ef8d
₣ⱠØⱤł₳₦ ₮ⱧɆ ₱ⱤØ <florianthepro@icloud.com>
2025-11-21 16:54:45 +0100

    Update and rename 404.html to 404.php

--- 404.php
+++ 404.php
@@ -1,3 +1,10 @@
+<?php
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header("Location: $redirect", true, 301);
+    exit();
+}
+?>
 <!doctype html>
 <html lang="de">
 <head>
